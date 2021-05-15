00bc0081cf2755f58f82a42150718478495c900b
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2021-05-15 16:21:54 -0300

    flat: "Organização do Projeto" O Projeto foi reorganizado com a estrutura de diretórios correta e foi configurado o ESLint.

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import "./App.css";
+
+const AppWrapper = styled.div`
+  text-align: left;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+function App() {
+  return (
+    <AppWrapper>
+      <AppHeader>
+        <AppLogo src="/images/logo.svg" alt="logo" title="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </AppLink>
+      </AppHeader>
+    </AppWrapper>
+  );
+}
+
+export default App;
